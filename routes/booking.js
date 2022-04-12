e22b6efbeefca2f59ccd0c10b64ef377f180e955
Yannapon Surak <62160058@go.buu.ac.th>
2022-04-12 22:05:25 +0700

    Add getBookingByUser

--- routes/booking.js
+++ routes/booking.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Booking = require('../models/Booking')
 
-router.get('/', async (req, res, next) => {
+const getByDate = async (req, res, next) => {
   try {
     const startDate = req.query.startDate
     const endDate = req.query.endDate
@@ -16,6 +16,21 @@ router.get('/', async (req, res, next) => {
       message: err.message
     })
   }
-})
+}
+
+const getBookingByUser = async (req, res, next) => {
+  try {
+    const id = req.params.id
+    const booking = await Booking.find({ user: id }).populate('room').populate('user').populate('approveres').exec()
+    res.status(200).json(booking)
+  } catch (err) {
+    return res.status(500).send({
+      message: err.message
+    })
+  }
+}
+
+router.get('/', getByDate)
+router.get('/users/:id', getBookingByUser)
 
 module.exports = router
