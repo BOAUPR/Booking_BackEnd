c7c958a5473088a77cc24e0de36d347b14d959a8
@62160249_jirat <62160249@go.buu.ac.th>
2022-04-15 00:32:26 +0700

    Add booking in updateStatus

--- routes/approver.js
+++ routes/approver.js
@@ -35,6 +35,7 @@ const updateStatus = async (req, res, next) => {
   try {
     const approver = await Approver.findById(pId)
     approver.status = req.body.status
+    approver.booking = req.body.booking
     await approver.save()
     return res.status(200).json(approver)
   } catch (err) {
